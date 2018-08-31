b4be8b4f9fd63433212cb27bfa96725e9dbcedbf
luomingqiang <luocq_123@163.com>
2018-08-31 21:41:34 +0800

    init

---
+++ src/main/java/com/lmq/study/thread/JavaThreadStudyApplication.java
@@ -0,0 +1,12 @@
+package com.lmq.study.thread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaThreadStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaThreadStudyApplication.class, args);
+	}
+}
