b3fcf8b0c497cf9022a768643c695d63515d032c
luomingqiang <luocq_123@163.com>
2018-09-01 23:18:11 +0800

    增加基于BlockingQueue的生产者消费者示例

--- src/main/java/com/lmq/study/thread/ch1/WaitAndNotify.java
+++ src/main/java/com/lmq/study/thread/ch1/WaitAndNotify.java
@@ -3,6 +3,9 @@ package com.lmq.study.thread.ch1;
 import java.time.LocalDateTime;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,6 +14,7 @@ public class WaitAndNotify {
 
 	private static Queue<LocalDateTime> queue = new LinkedList<>();
 	private static int MAX_SIZE = 10;
+	private static BlockingQueue<LocalDateTime> blockQueue = new LinkedBlockingQueue<>(10);
 	
 	public static class Producer implements Runnable {
 
@@ -78,10 +82,16 @@ public class WaitAndNotify {
 			while (queue.size() == 0) {
 				try {
 					log.info("the size is 0, consumer[{}] begin to wait", this.name);
+					long begin = System.currentTimeMillis();
 					queue.wait(500); // 带参数的wait和不带参数的wait？
-					log.info("consumer[{}]: 等了这么久都没有了，我不买了，回家了，反正一共买了{}个了", this.name, this.count);
-					running = false;
-					return; // 为什么不生产者不一样？为什么要这里要return？
+					long end = System.currentTimeMillis();
+					if((end - begin) >= 500) {
+						log.info("consumer[{}]: 等了这么久都没有了，我不买了，回家了，反正一共买了{}个了", this.name, this.count);
+						running = false;
+						return; // 为什么不生产者不一样？为什么要这里要return？
+					} else {
+						log.info("consumer[{}]: 我被唤醒了，看看是不是有东西可以买了", this.name);
+					}
 				} catch (InterruptedException e) {
 					// do nothing
 				}
@@ -90,6 +100,76 @@ public class WaitAndNotify {
 			LocalDateTime element = queue.poll();
 			log.info("consumer[{}] consume an element: {}, size: {}", this.name, element, queue.size());
 			queue.notifyAll();
+		}
+	}
+	
+	public static class Producer2 implements Runnable {
+
+		private String name;
+		private int count = 0;
+		private int max;
+		private volatile boolean running = true;
+		
+		public Producer2(String name, int max) {
+			this.name = name;
+			this.max = max;
+		}
+		
+		@Override
+		public void run() {
+			while(running) {
+				produce();
+				Thread.yield();
+				if(++count >= max) {
+					log.info("producer[{}] 生产了{}个了，下班了。", this.name, count);
+					running = false;
+				}
+			}
+		}
+		
+		private void produce() {
+			try {
+				LocalDateTime now = LocalDateTime.now();
+				blockQueue.put(now);
+				log.info("producer[{}] produce an new element: {}, size: {}", this.name, now, blockQueue.size());
+			} catch (InterruptedException e) {
+				// do nothing
+			}
+		}
+	}
+	
+	public static class Consumer2 implements Runnable {
+
+		private String name;
+		private int count;
+		private volatile boolean running = true;
+		
+		public Consumer2(String name) {
+			this.name = name;
+		}
+		
+		@Override
+		public void run() {
+			while(running) {
+				consume();
+				Thread.yield();
+			}
+		}
+
+		private void consume() {
+			try {
+				LocalDateTime element = blockQueue.poll(500, TimeUnit.MILLISECONDS);
+				if(element == null) {
+					log.info("consumer[{}]: 等了这么久都没有了，我不买了，回家了，反正一共买了{}个了", this.name, this.count);
+					running = false;
+					return;
+				} else {
+					count++;
+					log.info("consumer[{}] consume an element: {}, size: {}", this.name, element, blockQueue.size());
+				}
+			} catch (InterruptedException e) {
+				// do nothing
+			}
 		}
 	}
 }
--- src/test/java/com/lmq/study/thread/ch1/WaitAndNotifyTest.java
+++ src/test/java/com/lmq/study/thread/ch1/WaitAndNotifyTest.java
@@ -5,8 +5,11 @@ import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
+
 import com.lmq.study.thread.ch1.WaitAndNotify.Producer;
 import com.lmq.study.thread.ch1.WaitAndNotify.Consumer;
+import com.lmq.study.thread.ch1.WaitAndNotify.Producer2;
+import com.lmq.study.thread.ch1.WaitAndNotify.Consumer2;
 
 public class WaitAndNotifyTest {
 
@@ -21,6 +24,30 @@ public class WaitAndNotifyTest {
 		
 		for(int i = 0; i < 5; i++) {
 			Runnable consumer = new Consumer("c" + i);
+			Thread consumerT = WaysToCreateThread.byImplRunnable(consumer);
+			threads.add(consumerT);
+		}
+		
+		Collections.shuffle(threads);
+		for(Thread thread : threads) {
+			thread.start();
+		}
+		for(Thread thread : threads) {
+			thread.join();
+		}
+	}
+	
+	public @Test void testProducerAndConsumer2() throws InterruptedException {
+		
+		List<Thread> threads = new ArrayList<>(15);
+		for(int i = 0; i < 10; i++) {
+			Runnable producer = new Producer2("p" + i, 5);
+			Thread producerT = WaysToCreateThread.byImplRunnable(producer);
+			threads.add(producerT);
+		}
+		
+		for(int i = 0; i < 5; i++) {
+			Runnable consumer = new Consumer2("c" + i);
 			Thread consumerT = WaysToCreateThread.byImplRunnable(consumer);
 			threads.add(consumerT);
 		}
